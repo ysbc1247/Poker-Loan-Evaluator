0e6c4984c41a39fe0c7f52352900dd56b716dcf9
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 10:12:17 +0900

    Impl DTO

---
+++ Poker/src/main/java/com/projects/poker/dto/PlayerDTO.java
@@ -0,0 +1,14 @@
+package com.projects.poker.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlayerDTO {
+    private Long id;
+
+    private String name;
+
+    private Long cash;
+}
