9edfb56fed8ee914fa9595d756206bbfd2d18c71
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 14:25:21 +0900

    impl game cont

--- Poker/src/main/java/com/projects/poker/controller/GameController.java
+++ Poker/src/main/java/com/projects/poker/controller/GameController.java
@@ -1,4 +1,37 @@
 package com.projects.poker.controller;
 
+import com.projects.poker.dto.GameDTO;
+import com.projects.poker.dto.LoanDTO;
+import com.projects.poker.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
 public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @PostMapping
+    public ResponseEntity<GameDTO> startGame() {
+        GameDTO gameDTO = gameService.startGame();
+        return new ResponseEntity<>(gameDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GameDTO> endGame(@PathVariable(value = "id") Long id) {
+        GameDTO gameDTO = gameService.endGame(id);
+        return ResponseEntity.ok().body(gameDTO);
+    }
+
+    @GetMapping("/{id}/loans")
+    public ResponseEntity<List<LoanDTO>> getLoansByGameId(@PathVariable(value = "id") Long id) {
+        List<LoanDTO> loanDTOs = gameService.getLoansByGameId(id);
+        return ResponseEntity.ok().body(loanDTOs);
+    }
 }
