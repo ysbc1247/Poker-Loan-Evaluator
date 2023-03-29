243e48d4594615b9541d1c295e18be5635a660c0
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 10:12:39 +0900

    Impl service

---
+++ Poker/src/main/java/com/projects/poker/service/RequestService.java
@@ -0,0 +1,81 @@
+package com.projects.poker.service;
+
+import com.projects.poker.domain.Player;
+import com.projects.poker.domain.Request;
+import com.projects.poker.dto.RequestDTO;
+import com.projects.poker.repository.PlayerRepository;
+import com.projects.poker.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RequestService {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+    public RequestDTO createRequest(RequestDTO requestDTO) {
+        Player borrower = playerRepository.findById(requestDTO.getBorrowerId())
+                .orElseThrow(() -> new ResourceNotFoundException());
+        Player lender = playerRepository.findById(requestDTO.getLenderId())
+                .orElseThrow(() -> new ResourceNotFoundException());
+
+        Request request = new Request();
+        request.setAmount(requestDTO.getAmount());
+        request.setBorrower(borrower);
+        request.setLender(lender);
+
+        request = requestRepository.save(request);
+        return mapRequestToDTO(request);
+    }
+
+    public RequestDTO getRequestById(Long id) {
+        Request request = requestRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        return mapRequestToDTO(request);
+    }
+
+    public List<RequestDTO> getAllRequests() {
+        List<Request> requests = requestRepository.findAll();
+        return requests.stream()
+                .map(this::mapRequestToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public RequestDTO updateRequest(Long id, RequestDTO requestDTO) {
+        Request request = requestRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        Player borrower = playerRepository.findById(requestDTO.getBorrowerId())
+                .orElseThrow(() -> new ResourceNotFoundException());
+        Player lender = playerRepository.findById(requestDTO.getLenderId())
+                .orElseThrow(() -> new ResourceNotFoundException());
+        request.setAmount(requestDTO.getAmount());
+        request.setBorrower(borrower);
+        request.setLender(lender);
+        request = requestRepository.save(request);
+        return mapRequestToDTO(request);
+    }
+
+    public void deleteRequest(Long id) {
+        Request request = requestRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        requestRepository.delete(request);
+    }
+
+    private RequestDTO mapRequestToDTO(Request request) {
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setId(request.getId());
+        requestDTO.setAmount(request.getAmount());
+        requestDTO.setBorrowerId(request.getBorrower().getId());
+        requestDTO.setLenderId(request.getLender().getId());
+        return requestDTO;
+    }
+}
+
