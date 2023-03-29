b3f4d1842c233c7b622637cfa63d9a58cf64dbc4
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 13:29:08 +0900

    Impl entity game

---
+++ Poker/src/main/java/com/projects/poker/domain/Game.java
@@ -0,0 +1,36 @@
+package com.projects.poker.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL)
+    private List<Loan> loans = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "game_player",
+            joinColumns = @JoinColumn(name = "game_id"),
+            inverseJoinColumns = @JoinColumn(name = "player_id"))
+    private List<Player> players = new ArrayList<>();
+
+}
