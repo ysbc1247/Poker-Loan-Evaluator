f9243d460d4d4a2534ec80418dd74921e5deeef2
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 13:28:36 +0900

    Impl Response rep

--- Poker/src/main/java/com/projects/poker/repository/ResponseRepository.java
+++ Poker/src/main/java/com/projects/poker/repository/ResponseRepository.java
@@ -2,6 +2,8 @@ package com.projects.poker.repository;
 
 import com.projects.poker.domain.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ResponseRepository extends JpaRepository<Response, Long> {
 }
