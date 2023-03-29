be86796ae909513e91b764c2aead56aa09d5aebb
ysbc1247 <ystc1247@khu.ac.kr>
2023-03-29 16:44:21 +0900

    refactoring endpoints for controller

--- Poker/src/main/java/com/projects/poker/controller/GameController.java
+++ Poker/src/main/java/com/projects/poker/controller/GameController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/games")
+@RequestMapping("/games")
 public class GameController {
 
     @Autowired
