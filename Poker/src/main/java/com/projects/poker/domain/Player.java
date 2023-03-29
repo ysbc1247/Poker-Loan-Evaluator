de56b2435a21dbb7a09f8e2012296c97ce40ed45
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 10:12:05 +0900

    Impl entity

---
+++ Poker/src/main/java/com/projects/poker/domain/Player.java
@@ -0,0 +1,24 @@
+package com.projects.poker.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "player")
+
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cash")
+    private Long cash;
+
+
+}
