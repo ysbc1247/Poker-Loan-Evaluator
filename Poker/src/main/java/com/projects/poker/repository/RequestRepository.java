ec2529b794138a569e86faecd6519620a5957f4a
ystc1247 <ystc1247@khu.ac.kr>
2023-03-29 13:28:42 +0900

    Impl request rep

--- Poker/src/main/java/com/projects/poker/repository/RequestRepository.java
+++ Poker/src/main/java/com/projects/poker/repository/RequestRepository.java
@@ -2,6 +2,8 @@ package com.projects.poker.repository;
 
 import com.projects.poker.domain.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface RequestRepository extends JpaRepository<Request, Long> {
 }
