55f62d2da9930c9091aca4867d00c6e2bfd509da
ysbc1247 <ystc1247@khu.ac.kr>
2023-03-30 13:21:33 +0900

    Added socket dependency

--- Poker/src/main/java/com/projects/poker/config/WebSocketConfig.java
+++ Poker/src/main/java/com/projects/poker/config/WebSocketConfig.java
@@ -1,2 +1,22 @@
-package com.projects.poker.config;public class WebSocketConfig {
+package com.projects.poker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
 }
